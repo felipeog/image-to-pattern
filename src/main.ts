8f8af72cb52e5e864ee7f2e77fe4140400c81179
felipeog <17603069+felipeog@users.noreply.github.com>
2025-06-08 22:47:18 -0300

    add connected dots pattern

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ const imageOptions = [
   { textContent: "Skull 2", value: "/skull-2.png" },
 ];
 const patternOptions = [
+  { textContent: "Connected dots", value: "connected-dots" },
   { textContent: "Dice", value: "dice" },
   { textContent: "Diagonal dots", value: "diagonal-dots" },
   { textContent: "Dots", value: "dots" },
@@ -235,6 +236,112 @@ function handleFormSubmit(event: SubmitEvent) {
           circle.setAttribute("cy", String(row * height + height / 2 + offset));
 
           outputSvg.append(circle);
+        }
+      }
+    }
+
+    // TODO: original radius
+    if (pattern === "connected-dots") {
+      function getRadians(degrees: number) {
+        return degrees * (Math.PI / 180);
+      }
+      function getCos(degrees: number) {
+        return Math.cos(getRadians(degrees));
+      }
+      function getSin(degrees: number) {
+        return Math.sin(getRadians(degrees));
+      }
+
+      const radius = width * (1 / 3);
+
+      for (let row = 0; row < rowCount; row++) {
+        for (let col = 0; col < colCount; col++) {
+          const item = Math.round(matrix[row][col]);
+          if (item === 0) continue;
+
+          const circle = createSvgElement("circle");
+
+          circle.setAttribute("fill", foreground);
+          circle.setAttribute("r", String(radius));
+          circle.setAttribute(
+            "cx",
+            String(col * width + width * (1 / 2) + offset)
+          );
+          circle.setAttribute(
+            "cy",
+            String(row * height + height * (1 / 2) + offset)
+          );
+
+          outputSvg.append(circle);
+
+          const nextRowItem = Math.round(matrix?.[row + 1]?.[col]);
+          const nextColItem = Math.round(matrix?.[row]?.[col + 1]);
+
+          if (nextColItem === item) {
+            const path = createSvgElement("path");
+
+            path.setAttribute("fill", foreground);
+            path.setAttribute(
+              "d",
+              // prettier-ignore
+              [
+                "M",
+                `${col * width + width * (1 / 2) + radius * getCos(-45) + offset} `,
+                `${row * height + height * (1 / 2) + radius * getSin(-45) + offset} `,
+
+                "Q",
+                `${(col + 1) * width + offset} `,
+                `${row * height + height * (1 / 2) + offset} `,
+                `${(col + 1) * width + width * (1 / 2) + radius * getCos(-135) + offset} `,
+                `${row * height + height * (1 / 2) + radius * getSin(-45) + offset} `,
+
+                "L",
+                `${(col + 1) * width + width * (1 / 2) + radius * getCos(-135) + offset} `,
+                `${row * height + height * (1 / 2) + radius * getSin(45) + offset} `,
+
+                "Q",
+                `${(col + 1) * width + offset} `,
+                `${row * height + height * (1 / 2) + offset} `,
+                `${col * width + width * (1 / 2) + radius * getCos(45) + offset} `,
+                `${row * height + height * (1 / 2) + radius * getSin(45) + offset} `,
+              ].join("")
+            );
+
+            outputSvg.append(path);
+          }
+
+          if (nextRowItem === item) {
+            const path = createSvgElement("path");
+
+            path.setAttribute("fill", foreground);
+            path.setAttribute(
+              "d",
+              // prettier-ignore
+              [
+                "M",
+                `${col * width + width * (1 / 2) + radius * getCos(45) + offset} `,
+                `${row * height + height * (1 / 2) + radius * getSin(45) + offset} `,
+
+                "Q",
+                `${col * width + width * (1 / 2) + offset} `,
+                `${(row + 1) * height + offset} `,
+                `${col * width + width * (1 / 2) + radius * getCos(45) + offset} `,
+                `${(row + 1) * height + height * (1 / 2) + radius * getSin(-45) + offset} `,
+
+                "L",
+                `${col * width + width * (1 / 2) + radius * getCos(135) + offset} `,
+                `${(row + 1) * height + height * (1 / 2) + radius * getSin(-45) + offset} `,
+
+                "Q",
+                `${col * width + width * (1 / 2) + offset} `,
+                `${(row + 1) * height + offset} `,
+                `${col * width + width * (1 / 2) + radius * getCos(135) + offset} `,
+                `${row * height + height * (1 / 2) + radius * getSin(135) + offset} `,
+              ].join("")
+            );
+
+            outputSvg.append(path);
+          }
         }
       }
     }
