65c543adbab5af6a36cabe7421605006e0965d7e
felipeog <17603069+felipeog@users.noreply.github.com>
2025-06-09 22:41:51 -0300

    add patterns enum

--- src/patterns/types/index.ts
+++ src/patterns/types/index.ts
@@ -1,2 +1,3 @@
+export { PatternMap } from "./patternMap";
 export type { PatternParameters } from "./patternParameters";
 export type { PatternReturn } from "./patternReturn";
---
+++ src/patterns/types/patternMap.ts
@@ -0,0 +1,9 @@
+export enum PatternMap {
+  CONNECTED_DOTS_1 = "connected-dots-1",
+  CONNECTED_DOTS_2 = "connected-dots-2",
+  DIAGONAL_DOTS = "diagonal-dots",
+  DICE = "dice",
+  DOTS = "dots",
+  LINES = "lines",
+  TRIANGLES = "triangles",
+}
