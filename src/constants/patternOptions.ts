860873d106ccf5c111175acda9738e6b6a5d181f
felipeog <17603069+felipeog@users.noreply.github.com>
2025-06-09 22:42:11 -0300

    update pattern options

--- src/constants/patternOptions.ts
+++ src/constants/patternOptions.ts
@@ -1,9 +1,11 @@
+import { PatternMap } from "../patterns/types";
+
 export const patternOptions = [
-  { textContent: "Connected dots 1", value: "connected-dots-1" },
-  { textContent: "Connected dots 2", value: "connected-dots-2" },
-  { textContent: "Dice", value: "dice" },
-  { textContent: "Diagonal dots", value: "diagonal-dots" },
-  { textContent: "Dots", value: "dots" },
-  { textContent: "Lines", value: "lines" },
-  { textContent: "Triangles", value: "triangles" },
+  { textContent: "Connected dots 1", value: PatternMap.CONNECTED_DOTS_1 },
+  { textContent: "Connected dots 2", value: PatternMap.CONNECTED_DOTS_2 },
+  { textContent: "Diagonal dots", value: PatternMap.DIAGONAL_DOTS },
+  { textContent: "Dice", value: PatternMap.DICE },
+  { textContent: "Dots", value: PatternMap.DOTS },
+  { textContent: "Lines", value: PatternMap.LINES },
+  { textContent: "Triangles", value: PatternMap.TRIANGLES },
 ];
