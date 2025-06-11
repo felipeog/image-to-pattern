1c170f3b4cd7a6b666440a834885cce273c9df1a
felipeog <17603069+felipeog@users.noreply.github.com>
2025-06-10 22:10:03 -0300

    add generative noise option

--- src/constants/generativeOptions.ts
+++ src/constants/generativeOptions.ts
@@ -5,4 +5,5 @@ export const generativeOptions = [
   { textContent: "Noise 2", value: GenerativeMap.GENERATIVE_NOISE_2 },
   { textContent: "Noise 3", value: GenerativeMap.GENERATIVE_NOISE_3 },
   { textContent: "Noise 4", value: GenerativeMap.GENERATIVE_NOISE_4 },
+  { textContent: "Noise 5", value: GenerativeMap.GENERATIVE_NOISE_5 },
 ];
--- src/generative/types/generativeMap.ts
+++ src/generative/types/generativeMap.ts
@@ -3,4 +3,5 @@ export enum GenerativeMap {
   GENERATIVE_NOISE_2 = "generative-noise-2",
   GENERATIVE_NOISE_3 = "generative-noise-3",
   GENERATIVE_NOISE_4 = "generative-noise-4",
+  GENERATIVE_NOISE_5 = "generative-noise-5",
 }
