58b6087834ca88f618c5a1b7dd64ebf7b495ccca
felipeog <17603069+felipeog@users.noreply.github.com>
2025-06-12 15:31:49 -0300

    add water images

--- src/constants/imageOptions.ts
+++ src/constants/imageOptions.ts
@@ -4,4 +4,7 @@ export const imageOptions = [
   { textContent: "Skull 0", value: ImageMap.SKULL_0 },
   { textContent: "Skull 1", value: ImageMap.SKULL_1 },
   { textContent: "Skull 2", value: ImageMap.SKULL_2 },
+  { textContent: "Water 0", value: ImageMap.WATER_0 },
+  { textContent: "Water 1", value: ImageMap.WATER_1 },
+  { textContent: "Water 2", value: ImageMap.WATER_2 },
 ];
--- src/images/types/imageMap.ts
+++ src/images/types/imageMap.ts
@@ -2,4 +2,7 @@ export enum ImageMap {
   SKULL_0 = "/skull-0.png",
   SKULL_1 = "/skull-1.png",
   SKULL_2 = "/skull-2.png",
+  WATER_0 = "/water-0.jpeg",
+  WATER_1 = "/water-1.jpeg",
+  WATER_2 = "/water-2.jpeg",
 }
