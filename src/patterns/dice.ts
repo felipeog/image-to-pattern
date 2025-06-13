3bf5ad5b342b64182b8ca85f70f7804ac16ae8e9
felipeog <17603069+felipeog@users.noreply.github.com>
2025-06-13 15:33:43 -0300

    resolve dice todo

--- src/patterns/dice.ts
+++ src/patterns/dice.ts
@@ -1,7 +1,6 @@
 import { createSvgElement } from "../helpers";
 import type { PatternParameters, PatternReturn } from "./types";
 
-// TODO: create `defs` to reuse faces
 export function dice({
   matrix,
   width,
@@ -9,120 +8,168 @@ export function dice({
   offset,
   foreground,
 }: PatternParameters): PatternReturn {
-  const group = createSvgElement("g");
   const rowCount = matrix.length;
   const colCount = matrix[0].length;
+  const group = createSvgElement("g");
+  const defs = createSvgElement("defs");
 
-  function createCircle() {
-    const c = createSvgElement("circle");
+  defs.innerHTML = `
+    <g id="face1">
+      <circle
+        cx="${width * (1 / 2)}"
+        cy="${height * (1 / 2)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+    </g>
+    <g id="face2">
+      <circle
+        cx="${width * (1 / 4)}"
+        cy="${height * (1 / 4)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (3 / 4)}"
+        cy="${height * (3 / 4)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+    </g>
+    <g id="face3">
+      <circle
+        cx="${width * (1 / 4)}"
+        cy="${height * (1 / 4)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (1 / 2)}"
+        cy="${height * (1 / 2)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (3 / 4)}"
+        cy="${height * (3 / 4)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+    </g>
+    <g id="face4">
+      <circle
+        cx="${width * (1 / 4)}"
+        cy="${height * (1 / 4)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (3 / 4)}"
+        cy="${height * (1 / 4)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (3 / 4)}"
+        cy="${height * (3 / 4)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (1 / 4)}"
+        cy="${height * (3 / 4)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+    </g>
+    <g id="face5">
+      <circle
+        cx="${width * (1 / 4)}"
+        cy="${height * (1 / 4)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (3 / 4)}"
+        cy="${height * (1 / 4)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (1 / 2)}"
+        cy="${height * (1 / 2)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (3 / 4)}"
+        cy="${height * (3 / 4)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (1 / 4)}"
+        cy="${height * (3 / 4)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+    </g>
+    <g id="face6">
+      <circle
+        cx="${width * (1 / 4)}"
+        cy="${height * (1 / 3)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (1 / 2)}"
+        cy="${height * (1 / 3)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (3 / 4)}"
+        cy="${height * (1 / 3)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (1 / 4)}"
+        cy="${height * (2 / 3)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (1 / 2)}"
+        cy="${height * (2 / 3)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+      <circle
+        cx="${width * (3 / 4)}"
+        cy="${height * (2 / 3)}"
+        r="${width * 0.1}"
+        fill="${foreground}"
+      />
+    </g>
+  `;
 
-    c.setAttribute("fill", foreground);
-    c.setAttribute("r", String(width * 0.1));
-
-    return c;
-  }
+  group.append(defs);
 
   for (let col = 0; col < colCount; col++) {
     for (let row = 0; row < rowCount; row++) {
       const item = matrix[row][col];
-
-      const l = col * width; // left offset
-      const t = row * height; // top offset
-
       const face = Math.round(item * 6);
-      const c1 = createCircle();
-      const c2 = createCircle();
-      const c3 = createCircle();
-      const c4 = createCircle();
-      const c5 = createCircle();
-      const c6 = createCircle();
 
-      if (face === 1) {
-        c1.setAttribute("cx", String(l + width * (1 / 2) + offset));
-        c1.setAttribute("cy", String(t + height * (1 / 2) + offset));
+      if (face < 1 || face > 6) continue;
 
-        group.append(c1);
-      }
+      const use = createSvgElement("use");
 
-      if (face === 2) {
-        c1.setAttribute("cx", String(l + width * (1 / 4) + offset));
-        c1.setAttribute("cy", String(t + height * (1 / 4) + offset));
+      use.setAttribute("x", String(col * width + offset));
+      use.setAttribute("y", String(row * height + offset));
+      use.setAttribute("href", `#face${face}`);
 
-        c2.setAttribute("cx", String(l + width * (3 / 4) + offset));
-        c2.setAttribute("cy", String(t + height * (3 / 4) + offset));
-
-        group.append(c1, c2);
-      }
-
-      if (face === 3) {
-        c1.setAttribute("cx", String(l + width * (1 / 4) + offset));
-        c1.setAttribute("cy", String(t + height * (1 / 4) + offset));
-
-        c2.setAttribute("cx", String(l + width * (1 / 2) + offset));
-        c2.setAttribute("cy", String(t + height * (1 / 2) + offset));
-
-        c3.setAttribute("cx", String(l + width * (3 / 4) + offset));
-        c3.setAttribute("cy", String(t + height * (3 / 4) + offset));
-
-        group.append(c1, c2, c3);
-      }
-
-      if (face === 4) {
-        c1.setAttribute("cx", String(l + width * (1 / 4) + offset));
-        c1.setAttribute("cy", String(t + height * (1 / 4) + offset));
-
-        c2.setAttribute("cx", String(l + width * (3 / 4) + offset));
-        c2.setAttribute("cy", String(t + height * (1 / 4) + offset));
-
-        c3.setAttribute("cx", String(l + width * (3 / 4) + offset));
-        c3.setAttribute("cy", String(t + height * (3 / 4) + offset));
-
-        c4.setAttribute("cx", String(l + width * (1 / 4) + offset));
-        c4.setAttribute("cy", String(t + height * (3 / 4) + offset));
-
-        group.append(c1, c2, c3, c4);
-      }
-
-      if (face === 5) {
-        c1.setAttribute("cx", String(l + width * (1 / 4) + offset));
-        c1.setAttribute("cy", String(t + height * (1 / 4) + offset));
-
-        c2.setAttribute("cx", String(l + width * (3 / 4) + offset));
-        c2.setAttribute("cy", String(t + height * (1 / 4) + offset));
-
-        c3.setAttribute("cx", String(l + width * (1 / 2) + offset));
-        c3.setAttribute("cy", String(t + height * (1 / 2) + offset));
-
-        c4.setAttribute("cx", String(l + width * (3 / 4) + offset));
-        c4.setAttribute("cy", String(t + height * (3 / 4) + offset));
-
-        c5.setAttribute("cx", String(l + width * (1 / 4) + offset));
-        c5.setAttribute("cy", String(t + height * (3 / 4) + offset));
-
-        group.append(c1, c2, c3, c4, c5);
-      }
-
-      if (face === 6) {
-        c1.setAttribute("cx", String(l + width * (1 / 4) + offset));
-        c1.setAttribute("cy", String(t + height * (1 / 3) + offset));
-
-        c2.setAttribute("cx", String(l + width * (1 / 2) + offset));
-        c2.setAttribute("cy", String(t + height * (1 / 3) + offset));
-
-        c3.setAttribute("cx", String(l + width * (3 / 4) + offset));
-        c3.setAttribute("cy", String(t + height * (1 / 3) + offset));
-
-        c4.setAttribute("cx", String(l + width * (1 / 4) + offset));
-        c4.setAttribute("cy", String(t + height * (2 / 3) + offset));
-
-        c5.setAttribute("cx", String(l + width * (1 / 2) + offset));
-        c5.setAttribute("cy", String(t + height * (2 / 3) + offset));
-
-        c6.setAttribute("cx", String(l + width * (3 / 4) + offset));
-        c6.setAttribute("cy", String(t + height * (2 / 3) + offset));
-
-        group.append(c1, c2, c3, c4, c5, c6);
-      }
+      group.append(use);
     }
   }
 
